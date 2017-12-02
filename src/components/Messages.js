caee8b89b0a9cefd849b2cbdb89822f1af4c90b1
NNcrawler <rasyid.xyz@gmail.com>
2017-12-02 08:32:18 +0700

    initialize firebase problem

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -8,11 +8,73 @@ import {
 import {
   Divider,
 } from 'react-native-elements';
+import axios from 'axios';
+import fireapp from '@firebase/app';
+import '@firebase/firestore';
+// import '@firebase/app/dist/esm/src/firebaseApp';
+// import firestore from '@firebase/firestore';
+// import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCs2gd1l2vlZR0KBnUNVZ6SZRGam-J7S8",
+  authDomain: "i-quit-e703a.firebaseapp.com",
+  databaseURL: "https://i-quit-e703a.firebaseio.com",
+  projectId: "i-quit-e703a",
+  storageBucket: "i-quit-e703a.appspot.com",
+  messagingSenderId: "1018168285781"
+};
+fireapp.initializeApp(firebaseConfig);
+const db = fireapp.firestore();
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// firebaseApp.initializeApp(firebaseConfig);
 
 class Messages extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      messages: [],
+    }
   }
+
+  componentDidMount() {
+    this.setState({
+      messages: 'lll',
+    })
+
+    db.collection('messages').get().then((data) => {
+      alskjdpojw
+      this.setState({
+        messages: 'then',
+      })
+      alert('then')
+    })
+    .catch((err) => {
+      al;skjdl
+      this.setState({
+        messages: 'catch',
+      })
+      alert('catch')
+    })
+    console.log(db.collection('messages').get());
+    // alert('aasoy')
+  }
+
+  // ‎componentDidMount() {
+  //   // db.collection('messages').get().then((querySnapshot) => {
+  //   //   this.setState({
+  //   //     messages: 'asd',
+  //   //   })
+  //   // })
+  //   // .catch((err) => {
+  //   //   this.setState({
+  //   //     messages: 'asd'
+  //   //   })
+  //   // });
+  //   //
+  //   // this.setState({
+  //   //   messages: 'tes'
+  //   // })
+  // }
 
   render() {
     return (
@@ -41,6 +103,9 @@ class Messages extends Component {
           </Text>
           <Text style={styles.content}>
             {`${item.m}`}
+          </Text>
+          <Text>
+            {JSON.stringify(this.state.messages)}
           </Text>
           <Divider/>
         </View>
